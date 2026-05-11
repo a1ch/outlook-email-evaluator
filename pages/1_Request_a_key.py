90dc1b93fea00b592ff136f82447c7e59ecb4781
a1ch <shawn@ingotsolutions.com>
2026-05-11 15:46:06 -0600

    feat: create Request a Key page that calls run_key_request_page()

--- pages/1_Request_a_key.py
+++ pages/1_Request_a_key.py
@@ -1,5 +1,6 @@
-"""Streamlit multipage: sign-in and product key request (see portal/app.py)."""
-
-from portal.app import run_key_request_page
+"""
+Request a Product Key page
+"""
+from streamlit_app import run_key_request_page
 
 run_key_request_page()
